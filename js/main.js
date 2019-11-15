e3961b449f333d3e3a0c845b5b7ca251b3d9ce9b
Adam Pizzo <adampizzo@icloud.com>
2019-11-14 21:09:18 -0500

    reworking project from the ground up.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-let photos = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-// const $ul = $('<ul>', {class: "gallery-container"}).append();
+// let photos = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+// // const $ul = $('<ul>', {class: "gallery-container"}).append();
 
-for(let i=1; i < (photos.length+1); i++) {
-    if (i <= 9) {
-        $('.gallery-container').append('<li class="gallery-container-item"><img src="./photos/thumbnails/0' + i + '.jpg"></li>');
-    }
-    else
-    {
-        $('.gallery-container').append('<li class="gallery-container-item"><img src="./photos/thumbnails/' + i + '.jpg"></li>');
-    }
-  
-    console.log(i);
+let gallery = {
+    name: ["haybale"],
+    location:[],
+    caption: [],
 }
+
+var modal = document.getElementById("modal-box");
+var closeModalLeft = document.getElementById("leftarrow");
+var closeModalRight = document.getElementById("rightarrow");
+
+$(closeModalLeft).click(function() {
+    $(modal).hide();
+});
+
+$(closeModalRight).click(function() {
+    $(modal).hide();
+});
+
 
